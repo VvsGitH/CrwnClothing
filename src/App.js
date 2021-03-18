8a4b6e713c648f40d9627fadddfafdfcc8745cd4
Vito Paparella Santorsola <vs.paparella@gmail.com>
2021-03-18 16:34:11 +0100

    Implementata la ShopPage con i componenti collection-preview e collection-item

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Route } from "react-router";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
 	return (
 		<div>
 			<Route exact path='/' component={HomePage} />
+			<Route path='/shop' component={ShopPage} />
 		</div>
 	);
 }
