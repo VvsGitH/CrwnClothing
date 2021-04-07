bcdcad901cd07af4f95d50c382c76fd3c21ec0e6
Vito Paparella Santorsola <vs.paparella@gmail.com>
2021-04-07 17:32:43 +0200

    Aggiunta la lib redux-persist. Ora il carrello viene salvato nel localStorage del browser

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // LOCAL STORAGE
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+// CREO IL ROOT-REDUCER
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
 });
+
+// REDUX-PERSIST CONFIG
+// Salvo in locale solo il carrello
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['cart'],
+};
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import logger from 'redux-logger'; // Middleware
+import { persistStore } from 'redux-persist'; // Persistor
 
 import rootReducer from './root-reducer';
 
+// CREO LO STORE
 const middlewares = [logger];
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store;
+// REDUX-PERSIST
+export const persistor = persistStore(store);
