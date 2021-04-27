ed58e024801c104ecc2694976e8b117b93bfa0aa
Vito Paparella Santorsola <vs.paparella@gmail.com>
2021-04-27 16:47:09 +0200

    Convertito il progetto in una Progressive Web App: configurati correttamente index.html e manifest.json; attivato il service-worker in index.js; configurato l'utilizzo di ssl e del service-worker nel server express.

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import reportWebVitals from './reportWebVitals';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import App from './App';
 import { store, persistor } from './redux/store';
@@ -21,7 +23,7 @@ ReactDOM.render(
 	document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
--- server.js
+++ server.js
@@ -2,44 +2,61 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
-// Posso accedere alla secret key solo in dev e test!
-// In production, la chiave segreta sarà passata dal servizio di hosting
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-// Includo Stripe
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-// Creo una nuova app sulla port 5000 o sulla porta fornita dal servizio di deploy
+// Creo un nuovo server express sulla porta 5000 o su quella fornita da env
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Converto tutte le richieste in json in automatico
-// Controllo che non ci siano caratteri invalidi per un url
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+// Abilito il cross-origin HTTP request
+app.use(cors());
 // Imposto la compressione dei chunk js
 app.use(compression());
+// Converto tutte le richieste in json in automatico
+app.use(express.json());
+// Controllo che non ci siano caratteri invalidi per un url
+app.use(express.urlencoded({ extended: true }));
 
-// Server: port 5000
-// FrontEnd: port 3000
-// Devo abilitare le richieste dalla porta 3000 alla porta 5000
-app.use(cors());
+console.log('Initializing server on ' + process.env.NODE_ENV);
 
-// Dobbiamo servire il client
 if (process.env.NODE_ENV === 'production') {
+	// Inizializzo un server statico connesso a client/build
 	app.use(express.static(path.join(__dirname, 'client/build')));
 
-	app.get('*', (_req, res) => {
+	// Forzo la connessione https
+	// trustProtoHeader: true serve per il corretto funzionamento in Heroku
+	app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+	// Connetto il file index.html in client/build
+	app.get('/*', (_req, res) => {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 }
+
+// Imposto il path per il service worker del client
+app.get('service-worker.js', (_req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client/build', 'service-worker.js'));
+});
 
 // Metto il server in ascolto sulla porta specificata
 app.listen(port, error => {
 	if (error) throw error;
 	console.log('Server running on port ' + port);
 });
+
+/*
+########################
+
+	STRIPE CONFIG 
+
+########################
+*/
+
+// Inserisco la chiave segreta in process.env
+//process.env.NODE_ENV !== 'production' && require('dotenv').config();
+require('dotenv').config();
+// Inizializzo Stripe con la chiave segreta
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // Creo la payment route per la gestione dei pagamenti tramite Stripe
 // La request (ricevuta dal client) contiene il token per Stripe
